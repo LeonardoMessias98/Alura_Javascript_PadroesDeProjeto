1bcb85877b57d33000837284bca73c2f8e4d94f1
leonardomessias98 <messiasleonardo98@gmail.com>
2021-07-20 07:41:46 -0300

    feat(app): creating project structure

---
+++ client/js/lib/index.js
@@ -0,0 +1,29 @@
+const campos = [
+  document.querySelector("#data"),
+  document.querySelector("#quantidade"),
+  document.querySelector("#valor"),
+];
+
+const tbody = document.querySelector("table tbody");
+
+document.querySelector(".form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const tr = document.createElement("tr");
+
+  campos.forEach((campo) => {
+    const td = document.createElement("td");
+    td.textContent = campo.value;
+    tr.appendChild(td);
+  });
+
+  const tdVolume = document.createElement("td");
+  tdVolume.textContent = campos[1].value * campos[2].value;
+
+  tr.appendChild(tdVolume);
+  tbody.appendChild(tr);
+
+  campos[0].value = "";
+  campos[0].value = 1;
+  campos[0].value = 0;
+});
